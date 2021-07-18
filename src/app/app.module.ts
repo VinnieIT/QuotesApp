60b36771d67a3446c747076ce36df8afd379e2c9
Vincent <vincent.mwangi@student.moringaschool.com>
2021-07-18 20:39:46 +0300

    This are the initial test files for typescript for thr directive and compom[nenets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { DateDiffPipe } from './date-diff.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuoteDetailsComponent,
+    QuoteFormComponent,
+    DateDiffPipe
   ],
   imports: [
     BrowserModule,
