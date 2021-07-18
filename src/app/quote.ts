2340f4aadf3d2efb93516b766d3b7395aa58d0bb
Vincent <vincent.mwangi@student.moringaschool.com>
2021-07-19 00:10:36 +0300

    The quote input output data binding

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,10 @@
 export class Quote {
+
+
+    showDetails: boolean;
+
+    constructor(public id: number, public topic: string, public postedBy: string,
+         public description: string, public quoteBy: string,public postedOn: Date){
+            this.showDetails = false;
+         }
 }
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -10,14 +10,15 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
 
   quotes:Quote[] =[
-    new Quote(1, 'Stocism','Alfred','The greatest fear is the fear for fear', 'vinci', new Date(2021,7,17)),
+    new Quote(1, 'Stoicism and Hard','Alfred','The greatest fear is the fear for fear', 'vinci', new Date(2021,7,17)),
     new Quote(2, 'Stocism','Bob','The greatest fear is the fear for fear', 'Aurelius', new Date(2021,7,17)),
     new Quote(3, 'Stocism','Alice','The greatest fear is the fear for fear', 'Epictetus', new Date(2021,7,17)),
     new Quote(4, 'Stocism','Job','The greatest fear is the fear for fear', 'Plato', new Date(2021,7,17)),
     new Quote(5, 'Stocism','Mark','The greatest fear is the fear for fear', 'Socrates', new Date(2021,7,17)),
     new Quote(6, 'Stocism','Ray','The greatest fear is the fear for fear', 'Darwin', new Date(2021,7,17)),
     new Quote(7, 'Stocism','Mohammed','The greatest fear is the fear for fear', 'Gorbachev', new Date(2021,7,17)),
-  ];
+  
+    ];
 
 
   constructor() { }
