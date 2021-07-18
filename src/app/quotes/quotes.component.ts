152c8cda5a28f98f51f23f8224f0e3f16bb0bace
Vincent <vincent.mwangi@student.moringaschool.com>
2021-07-19 00:03:44 +0300

    THe quote component styling

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
+
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quotes:Quote[] =[
+    new Quote(1, 'Stocism','Alfred','The greatest fear is the fear for fear', 'vinci', new Date(2021,7,17)),
+    new Quote(2, 'Stocism','Bob','The greatest fear is the fear for fear', 'Aurelius', new Date(2021,7,17)),
+    new Quote(3, 'Stocism','Alice','The greatest fear is the fear for fear', 'Epictetus', new Date(2021,7,17)),
+    new Quote(4, 'Stocism','Job','The greatest fear is the fear for fear', 'Plato', new Date(2021,7,17)),
+    new Quote(5, 'Stocism','Mark','The greatest fear is the fear for fear', 'Socrates', new Date(2021,7,17)),
+    new Quote(6, 'Stocism','Ray','The greatest fear is the fear for fear', 'Darwin', new Date(2021,7,17)),
+    new Quote(7, 'Stocism','Mohammed','The greatest fear is the fear for fear', 'Gorbachev', new Date(2021,7,17)),
+  ];
+
 
   constructor() { }
 
