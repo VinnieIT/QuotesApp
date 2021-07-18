a268958607af5c79ab1d2ebbca995c93b0269d67
Vincent <vincent.mwangi@student.moringaschool.com>
2021-07-19 00:05:59 +0300

    The quote details delete button functionality

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
+import { Quote } from  '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
+
+  @Input() quote!:Quote;
+  @Output() isDone =new EventEmitter<boolean>();
+
+  removeQuote(done:boolean){
+    this.isDone.emit(done);
+  }
 
   constructor() { }
 
