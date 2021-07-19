f16898c5314007491ed2a5524dc312e322e40aaf
VinnieIT <mwangivin3@gmail.com>
2021-07-19 15:51:47 +0300

    Added the upvotes and downvotes attributes in the class constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,7 +4,7 @@ export class Quote {
     showDetails: boolean;
 
     constructor(public id: number, public topic: string, public postedBy: string,
-         public description: string, public quoteBy: string,public postedOn: Date){
+         public description: string, public quoteBy: string, public upvotes: number,public downvotes: number, public postedOn: Date, ){
             this.showDetails = false;
          }
 }
