43e38cdb25270132e2de9bc20613392e7b525c39
VinnieIT <mwangivin3@gmail.com>
2021-07-19 15:57:32 +0300

    Upvote and downvote event binding in the component

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
 import { Quote } from '../quote';
 
 
@@ -8,17 +8,55 @@ import { Quote } from '../quote';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+  @Input() quote:Quote = new Quote(0,"","","","",0,0,new Date,);
+  @Output() increament = new EventEmitter <number>();
+
 
   quotes:Quote[] =[
-    new Quote(1, 'Stoicism and Hard','Alfred','The greatest fear is the fear for fear', 'vinci', new Date(2021,7,17)),
-    new Quote(2, 'Stocism','Bob','The greatest fear is the fear for fear', 'Aurelius', new Date(2021,7,17)),
-    new Quote(3, 'Stocism','Alice','The greatest fear is the fear for fear', 'Epictetus', new Date(2021,7,17)),
-    new Quote(4, 'Stocism','Job','The greatest fear is the fear for fear', 'Plato', new Date(2021,7,17)),
-    new Quote(5, 'Stocism','Mark','The greatest fear is the fear for fear', 'Socrates', new Date(2021,7,17)),
-    new Quote(6, 'Stocism','Ray','The greatest fear is the fear for fear', 'Darwin', new Date(2021,7,17)),
-    new Quote(7, 'Stocism','Mohammed','The greatest fear is the fear for fear', 'Gorbachev', new Date(2021,7,17)),
+    new Quote(1, 'Stoicism and Life','Alfred','Waste no more time arguing what a good man should be. Be One.', 'Marcus Aurelius',0,0, new Date(2021,7,17)),
+    new Quote(2, 'Stoicism and Life','Bob','He who fears death will never do anything worth of a man who is alive.', 'Seneca', 0,0,new Date(2021,7,17)),
+    new Quote(3, 'Stoicism and Life','Alice','Luck is what happens when preparation meets opportunity.', 'Epictetus', 0,0,new Date(2021,7,17)),
+    new Quote(4, 'Stoicism and Life','Job','How long are you going to wait before you demand the best for yourself?', 'Epictetus', 0,0,new Date(2021,7,17)),
+    new Quote(5, 'Stoicism and Life','Mark','Hang on to your youthful enthusiasms — you’ll be able to use them better when you’re older.', 'Seneca',0,0, new Date(2021,7,17)),
+    new Quote(6, 'Stoicism and Life','Ray','The happiness of your life depends upon the quality of your thoughts.', 'Marcus Aurelius', 0,0,new Date(2021,7,17)),
+    new Quote(7, 'Stoicism and Life','Mohammed','Wealth consists not in having great possessions, but in having few wants.', 'Epictetus',0,0, new Date(2021,7,17)),
   
-    ];
+  ];
+  showDetail(index: number){
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
+  }
+  removeQuote(isDone: any, index:number){
+    if (isDone) {
+      let toRemove = confirm(`Are you sure you want to delete ${this.quotes[index].topic}?`)
+      if (toRemove) {
+        this.quotes.splice(index, 1)
+        
+      }
+      
+    }
+
+  }
+  addVotes (index:number){
+    this.quotes[index].upvotes+=1;
+  }
+
+  addDisvotes (index:number){
+    console.log(this.quotes[index].downvotes+=1);
+
+
+  }
+
+  addNewQuote(quote: Quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.postedOn = new Date(quote.postedOn)
+    this.quotes.push(quote)
+
+  }  
+
+
+
+
 
 
   constructor() { }
