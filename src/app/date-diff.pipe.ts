f5b380280d044e4a0371dcff4d16fc33d19040e2
VinnieIT <mwangivin3@gmail.com>
2021-07-19 15:09:39 +0300

    THe pipe logic to count days since the quote was published

--- src/app/date-diff.pipe.ts
+++ src/app/date-diff.pipe.ts
@@ -4,9 +4,24 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'dateDiff'
 })
 export class DateDiffPipe implements PipeTransform {
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
+    
+    var dateDifference = Math.abs(value - todayWithNoTime)
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference * 0.001;
+    var dateCounter =dateDifferenceSeconds/secondsInDay;
+
+    if (dateCounter >= 1 && value > todayWithNoTime) {
+      return dateCounter;
+      
+    }else{
+    return 0;
+    }
   }
 
 }
+
+  
